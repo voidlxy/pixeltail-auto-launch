3ec164458b5969526a9527bf60458f58782a0039
voidlxy <eilog@outlook.com>
2023-09-13 22:49:17 +0800

    update AppController

--- src/main/java/org/front/AppController.java
+++ src/main/java/org/front/AppController.java
@@ -1,6 +1,6 @@
 package org.front;
 //====================================================================================
-//PIXELTAIL-AutoLaunch-AppLaunch
+//PIXELTAIL-AutoLaunch-AppController
 //为像素尾巴启动器前端提供控制器
 //====================================================================================
 //------------------------------------------------------------------------------------
@@ -14,7 +14,9 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.launcher.Main;
 import org.to2mbn.jmccc.launch.LaunchException;
-import java.io.IOException;
+import org.yaml.snakeyaml.Yaml;
+import java.io.*;
+import java.util.Map;
 //------------------------------------------------------------------------------------
 //声明AppController公共类
 //------------------------------------------------------------------------------------
@@ -63,7 +65,7 @@ public class AppController {
         //配置fxml加载器并从helpFront.xml加载控件布局
         FXMLLoader helpFxmlLoader = new FXMLLoader(AppController.class.getResource("helpFront.fxml"));
         //创建场景并加入布局
-        Scene helpScene = new Scene(helpFxmlLoader.load(), 854, 480);
+        Scene helpScene = new Scene(helpFxmlLoader.load(), 570, 320);
         //----------------------------------------------------------------------------
         //配置窗口设置
         //----------------------------------------------------------------------------
@@ -76,5 +78,58 @@ public class AppController {
         //----------------------------------------------------------------------------
         helpStage.show();
     }
-}
 
+    public void initialize() throws IOException {
+        //----------------------------------------------------------------------------
+        //重写初始化方法，读配置文件
+        //----------------------------------------------------------------------------
+        //实例化File对象
+        File configFile = new File("config.yml");
+        //----------------------------------------------------------------------------
+        //判断配置文件是否存在
+        //----------------------------------------------------------------------------
+        if(configFile.exists()){
+            //如果存在，直接开始读取
+            // 创建一个YAML对象
+            Yaml yaml = new Yaml();
+            // 使用FileInputStream加载YAML文件
+            InputStream inputStream = new FileInputStream("config.yml");
+            // 解析YAML文件内容为Map对象
+            Map<String, Object> data = yaml.load(inputStream);
+            //------------------------------------------------------------------------
+            //判断配置文件是否为空
+            //------------------------------------------------------------------------
+            if (data == null || data.isEmpty()) {
+                //若为空，报错并启动帮助页面
+                System.out.println("Data is empty");
+                onHelpButtonClick();
+            }else{
+                //若不空，解析yaml文件
+                String userName = (String) data.get("userName");
+                String passWord = (String)data.get("passWord");
+                String gameVersion = (String) data.get("gameVersion");
+                String rootDir = (String)data.get("rootDir");
+                int maxMemory = (int) data.get("maxMemory");
+                int windowWidth = (int) data.get("windowWidth");
+                int windowHeight = (int) data.get("windowHeight");
+                String jreDir = (String)data.get("jreDir");
+                String mcRuntimeDir = (String) data.get("mcRuntimeDir");
+                String javaAgent = (String)data.get("javaAgent");
+                //将yaml配置文件内容显示到输入框
+                UserName.setText(userName);
+                PassWord.setText(passWord);
+                Version.setText(gameVersion);
+                RootDir.setText(rootDir);
+                MaxMemory.setText(String.valueOf(maxMemory));
+                Width.setText(String.valueOf(windowWidth));
+                Height.setText(String.valueOf(windowHeight));
+                JreDir.setText(jreDir);
+                McRuntimeDir.setText(mcRuntimeDir);
+                JavaAgent.setText(javaAgent);
+            }
+        }else{
+            //若不存在，启动帮助页面
+            onHelpButtonClick();
+        }
+    }
+}
